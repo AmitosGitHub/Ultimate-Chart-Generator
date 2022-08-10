c9244ec65b9c4b09d4024c335316af176f5b0578
AmitosGitHub <amit.osman@gmail.com>
2022-08-10 14:22:40 +0300

    first

---
+++ js/controller/app.controller.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function onInit() {
+  console.log('hjef')
+}
+
+function onToggle() {
+  document.querySelector('.container-generator').classList.toggle('hide')
+  document.querySelector('.container-gallery').classList.toggle('hide')
+}
